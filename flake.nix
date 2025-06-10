f446055773f1521fa478d42b10f139d648b959ba
Sven Eller <sven.eller@pm.me>
2025-06-10 18:45:55 +0300

    enable flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My NixOS system configuration (flake)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
